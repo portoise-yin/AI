d76c6a309f59c1435eba70ca49d9edffa4f720f7
portoise-yin <60853908+portoise-yin@users.noreply.github.com>
2021-07-24 19:20:25 -0400

    adding files
    manually adding these files because desktop doesn't want to I guess

---
+++ beginning_code.py
@@ -0,0 +1,11 @@
+#this function will read in images
+def read_in(file_name):
+	print("text text")
+
+def main():
+	file_name_str = "images_by_url.txt"
+	read_in(file_name_str)
+	print("test text")
+
+if __name__ == '__main__':
+	main()
