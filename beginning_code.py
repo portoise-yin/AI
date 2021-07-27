14b7db63b069e66add49d3e8bb27b4302afe5316
portoise-yin <fuzzypeachfuzz@gmail.com>
2021-07-27 15:32:38 -0400

    uploaded color.csv, boxsdk
    trying to mess around with boxsdk to get the program to read in from box folder

--- beginning_code.py
+++ beginning_code.py
@@ -1,6 +1,12 @@
+# will need to !pip install boxsdk
+import "colors.csv"
+from io import StringIO
+import pandas as pd
+from boxsdk import Client, OAuth2
+
 #this function will read in images
 def read_in(file_name):
-	print("text text")
+	print("")
 
 def main():
 	file_name_str = "images_by_url.txt"
